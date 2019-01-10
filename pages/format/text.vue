5d6d60b004ad009e52300f8f87c7305aa653679a
dezodev <dezodev@gmail.com>
2019-01-10 23:42:05 +0100

    DEV 20190110

--- pages/format/text.vue
+++ pages/format/text.vue
@@ -6,10 +6,21 @@
 
 		<formTextareaInput fieldName="text-transform" fieldLabel="Texte transformé" v-model="textTransform" />
 
-		<div class="actions">
+		<div class="btn-actions">
 			<va-button @click="textExchange">
 				<i class="fas fa-exchange-alt"></i> Echanger
 			</va-button>
+			<va-dropdown :click-close="true">
+				<div slot="trigger">
+					<va-button>
+						Casse des lettres
+						<va-icon type="angle-down" margin="0 0 0 7px"></va-icon>
+					</va-button>
+				</div>
+				<li><a @click="changeLettercase('lower')">Minuscule</a></li>
+				<li><a @click="changeLettercase('upper')">Majuscule</a></li>
+				<li><a @click="changeLettercase('capitalize')">Mettre une majuscule le premier mot</a></li>
+			</va-dropdown>
 		</div>
 	</section>
 </template>
@@ -32,6 +43,22 @@ export default {
 			this.textOrigin = thisTransform;
 			this.textTransform = thisOrigin;
 		},
+		changeLettercase (mode) {
+			if (this.textOrigin) {
+				switch (mode) {
+				case "lower":
+					this.textTransform = this.textOrigin.toLowerCase()
+					break;
+				case "upper":
+					this.textTransform = this.textOrigin.toUpperCase()
+					break;
+				case "capitalize":
+					this.textTransform = this.textOrigin.toLowerCase()
+					this.textTransform[0] = this.textTransform[0].toUpperCase();
+					break;
+				}
+			}
+		}
 	}
 }
 </script>
